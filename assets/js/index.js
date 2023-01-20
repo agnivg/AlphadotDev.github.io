8a22514ce3a5798daa2d21ec7f09b0f1a985b798
Dibyajyoti Mondal <dibyajyoti.bhs@gmail.com>
2023-01-20 18:29:20 +0530

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+// swiper js
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 70,
+      stretch: 0,
+      depth: 600,
+      modifier: 1,
+      slideShadows: true,
+    },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  });
